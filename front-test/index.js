365bfbf1ed7bcfff1aac941758fe68f643c792b8
dygy <yukimurachris@gmail.com>
2019-10-26 22:27:26 +0300

    init commit

---
+++ front-test/index.js
@@ -0,0 +1,46 @@
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require("path");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+}));
+app.get('/test', function(req, res, next) {
+    res.render('./index', { title: 'Auth' });
+});
+app.get('/play', function(req, res, next) {
+    res.render('./play', { artist: 'Auth',title:"" });
+});
+app.get('/b1', function(req, res, next) {
+    res.render('./b1', { title: 'Auth' });
+});
+app.get('/b2', function(req, res, next) {
+    res.render('./b2', { title: 'Auth' });
+});
+app.get('/b3', function(req, res, next) {
+    res.render('./b3', { artist: 'Auth',title:"" });
+});
+app.get('/b4', function(req, res, next) {
+    res.render('./b4', { title: 'Auth' });
+});
+app.get('/b5', function(req, res, next) {
+    res.render('./b5', { title: 'Auth' });
+});
+app.get('/b6', function(req, res, next) {
+    res.render('./b6', { artist: 'Auth',title:"" });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+server.listen(process.env.PORT || 3000,'0.0.0.0', () => console.log('Example app listening on port 3000!'));
---
+++ front-test/public/heart.js
@@ -0,0 +1,86 @@
+UpdateCanvas = function (func){
+    window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.amozRequestAnimationFrame || (function (func){setTimeout(func, 16.666);});
+    window.requestAnimationFrame(func);
+};
+
+let width = 0;
+Animate = function ()
+{
+    const ca = document.getElementById("cvs3");
+    const co = ca.getContext('2d');
+
+    // Create gradient
+    ca.width = ca.width;
+
+    co.save();
+
+    // Before drawing the line the canvas should be clipped
+    co.beginPath;
+    co.rect(0,0,width,250)
+    co.clip();
+
+    co.beginPath();
+    co.lineWidth = 3;
+    co.lineCap = 'round';
+    co.strokeStyle ="#fdfbff";
+    const coords = [
+        {x: 10, y: 200}, {x: 50, y: 200},
+        {x: 60, y: 125}, {x: 75, y: 225},
+        {x: 85, y: 175}, {x: 95, y: 205},
+        {x: 105, y: 150}, {x: 115, y: 210},
+        {x: 125, y: 190}, {x: 135, y: 208},
+        {x: 145, y: 125}, {x: 155, y: 215},
+        {x: 165, y: 175}, {x: 175, y: 205},
+        {x: 185, y: 115}, {x: 195, y: 220},
+        {x: 205, y: 150}, {x: 215, y: 215},
+        {x: 225, y: 190}, {x: 235, y: 210},
+        {x: 245, y: 115}, {x: 255, y: 212},
+        {x: 265, y: 200}, {x: 400, y: 200},
+        {x: 405, y: 190}, {x: 415, y: 210},
+        {x: 425, y: 150}, {x: 435, y: 210},
+        {x: 445, y: 115}, {x: 455, y: 225},
+        {x: 465, y: 200}, {x: 540, y: 200}];
+
+    for (let i=0; i<coords.length; i++) {
+        //co.moveTo(coords[i].x, coords[i].y);
+        co.lineTo(coords[i].x, coords[i].y);
+    }
+
+    co.stroke();
+    co.restore();
+    co.fillStyle="#FF0000";
+    if (width < 600) {
+        width += 1;
+        co.fillStyle="#FF0000";
+        UpdateCanvas(Animate);
+
+        // Reset the width to zero to allow the button to be pressed again
+    } else {
+        width = 0;
+    }
+};
+let x;
+
+function hard() {
+    x = 10000;
+    let speed = 10;
+    for (speed; speed > 0; speed--) {
+        play()
+    }
+}
+function slow() {
+    x = 3000;
+    while (x<5000){
+        setTimeout(()=>{
+            Animate();
+            x+=1000
+        },x)
+    }
+}
+Animate();
+slow();
+function play(){
+    setInterval(()=>{
+        Animate();
+    },x);
+}
